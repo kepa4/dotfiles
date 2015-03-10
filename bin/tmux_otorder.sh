7f73275fdf6cd9add48f3c24fcaf538f434bb8d3
cjeffers <cwjeffers18@gmail.com>
2015-03-10 15:26:43 -0600

    Check for tmux session, update background task

--- bin/tmux_otorder.sh
+++ bin/tmux_otorder.sh
@@ -1,36 +1,54 @@
-#! /bin/sh
-tmux -2 new-session -d -s otorder
+#! /bin/bash
+NAME='otorder'
+SUCCESS=0
+WAIT_SECS=1
 
-### localhost window
-# server pane
-tmux rename-window -t otorder:1 'localhost'
-tmux send-keys "cd ~/Documents/dev/rankomatic/ && bash bin/run_dev_server.sh" C-m
+tmux attach -t $NAME
+OUT=$?
 
-# rqworker panes
-tmux split-window
-tmux select-pane -t 1
-tmux send-keys "cd ~/Documents/dev/rankomatic/ && workon ot_orders && rqworker" C-m
+if [ $OUT -eq $SUCCESS ]; then
+    exit
+else
+    tmux -2 new-session -d -s $NAME
 
-tmux split-window -h
-tmux select-pane -t 2
-tmux send-keys "workon ot_orders && rqinfo" C-m
+    ### localhost window
+    tmux rename-window -t $NAME:1 'localhost'
+
+    # rqworker panes
+    tmux split-window
+    tmux select-pane -t 1
+    tmux send-keys "cd ~/Documents/dev/rankomatic/" C-m
+    tmux send-keys "workon otorder" C-m
+    tmux send-keys "./otorderd_logd.py start" C-m
+    sleep $WAIT_SECS
+    tmux send-keys "./otorderd start" C-m
+    sleep $WAIT_SECS
+
+    # terminal pane
+    tmux split-window -h
+
+    # server pane
+    tmux select-pane -t 0
+    tmux send-keys "cd ~/Documents/dev/rankomatic/" C-m
+    tmux send-keys "bash rankomatic/bin/run_dev_server.sh" C-m
 
 
-### editing window
-tmux new-window -t otorder:2 -n 'editing'
-tmux send-keys "cd ~/Documents/dev/rankomatic/rankomatic" C-m
-tmux split-window -h
-tmux select-pane -t 0
-tmux resize-pane -x 85
-tmux select-pane -t 1
-tmux split-window
+    ### editing window
+    tmux new-window -t $NAME:2 -n 'editing'
+    tmux send-keys "cd ~/Documents/dev/rankomatic/rankomatic" C-m
+    tmux split-window -h
+    tmux select-pane -t 0
+    tmux resize-pane -x 85
+    tmux select-pane -t 1
+    tmux split-window
 
 
-### otorder.com window
-tmux new-window -t otorder:3 -n 'otorder.com'
-tmux send-keys "otorder-connect" C-m
+    ### otorder.com window
+    tmux new-window -t $NAME:3 -n 'otorder.com'
+    tmux send-keys "otorder-connect" C-m
 
 
-### select default window and attach
-tmux select-window -t otorder:2
-tmux -2 attach-session -t otorder
+    ### select default window and attach
+    tmux select-window -t $NAME:2
+    tmux -2 attach-session -t $NAME
+fi
